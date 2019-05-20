af44d9213ef31824a043055a7c7e6e516e33f215
nowayoutbut <urar.akkun@gmail.com>
2019-05-20 20:22:17 +0900

    メソッドを分割してファイル名を変更

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
 from functional import seq
 import random
-
-symbols = ["Heart", "Spade", "Diamond", "Clover"]
-points = [
-    {"num":"A","point":11}, 
-    {"num":"K","point":10},
-    {"num":"Q","point":10},
-    {"num":"J","point":10},
-    {"num":"10","point":10},
-    {"num":"9","point":9},
-    {"num":"8","point":8},
-    {"num":"7","point":7},
-    {"num":"6","point":6},
-    {"num":"5","point":5},
-    {"num":"4","point":4},
-    {"num":"3","point":3},
-    {"num":"2","point":2},
-]
-
-
-def createCards(symbol):
-    return seq(points).map(lambda point: {**point, "symbol": symbol})
-
-cards = seq(symbols).map(createCards).flatten().to_list()
-random.shuffle(cards)
+from cards import cards
 
 def drawCard(count=1):
     pick = []
